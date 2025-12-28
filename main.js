e4e42a70aa8f95ab3ca0ea1f833ccde138d60b02
Joaoc <joaoc86@gmail.com>
2025-12-28 16:46:10 +0000

    generate a hard-coded grid of 16x16 square divs

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".sketchpad");
 
 // create divs
 
@@ -8,6 +8,6 @@ function createSquare() {
   container.appendChild(square);
 }
 
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 16 * 16; i++) {
   createSquare();
 }
